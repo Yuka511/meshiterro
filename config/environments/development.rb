39702c0b04d5f2b81e1aaf7496df4596e306f625
Yuka511 <174612970+Yuka511@users.noreply.github.com>
2024-08-22 09:33:41 +0000

    [Finish]4-5

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.active_job.queue_adapter = inline
+  config.active_job.queue_adapter = :inline
 
   config.hosts << "96913f1ec82f4caba1291fa6956f4298.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
